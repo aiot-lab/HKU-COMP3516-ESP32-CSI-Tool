e28ae034fe239ff23760f1b0b1184fd165f3ac68
Hou Weiying <weiying_hou@outlook.com>
2023-03-17 14:55:18 +0800

    clean start_monitor.sh

--- active_sta/start_monitor.sh
+++ active_sta/start_monitor.sh
@@ -1,3 +1,1 @@
 #!/bin/bash
-export IDF_PATH=/Users/Halloween/esp/esp-idf
-/Users/Halloween/.espressif/python_env/idf4.4_py3.7_env/bin/python /Users/Halloween/esp/esp-idf/tools/idf_monitor.py -p /dev/cu.usbserial-10 -b 115200 --toolchain-prefix xtensa-esp32s3-elf- --target esp32s3 /Users/Halloween/Documents/Octosense/esp32-csi-tool/active_sta/build/active-sta.elf
